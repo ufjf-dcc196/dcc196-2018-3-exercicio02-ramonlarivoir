553f44c4b052f3acfcd75787eab054b524e6fb13
ICE <ice@lab05m44.nrc.ice.ufjf.br>
2018-10-10 20:28:36 -0300

    Adicionando ação nos botoẽs

--- app/src/main/java/com/example/ice/dcc192_exercicio2/CadastroActivity.java
+++ app/src/main/java/com/example/ice/dcc192_exercicio2/CadastroActivity.java
@@ -1,13 +1,32 @@
 package com.example.ice.dcc192_exercicio2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText edtTitulo;
+    private EditText edtEpisodio;
+    private EditText edtTemporada;
+    private Button btnCadastrar;
+    private Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        btnVoltar = (Button) findViewById(R.id.btn_voltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CadastroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnCadastrar = (Button) findViewById(R.id.btn_cadastrar);
     }
 }
--- app/src/main/java/com/example/ice/dcc192_exercicio2/MainActivity.java
+++ app/src/main/java/com/example/ice/dcc192_exercicio2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.ice.dcc192_exercicio2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnAdicionar;
+    private RecyclerView rclSeries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdicionar = (Button) findViewById(R.id.btn_add);
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
